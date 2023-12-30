64a8b3adafc372c38a4ac7e5a5618edff9c15f58
Dariusz Podczasik <boostertech@mail.com>
2023-12-30 14:23:12 +0000

    fetchRates function moved to seperate file(api.js)

---
+++ src/Form/api.js
@@ -0,0 +1,23 @@
+export const fetchRates = async () => {
+  try {
+    const response = await fetch(
+      "https://api.currencyapi.com/v3/latest?apikey=cur_live_H22cRxqJXhrapwztireTdTdMjbhrkl78Z507fGPh&currencies=USD%2CEUR%2CJPY%2CGBP%2CAUD%2CCAD%2CCHF%2CCNY%2CSEK%2CNZD%2CHKD%2CNOK%2CKRW%2CTRY%2CMXN%2CSGD%2CINR%2CRUB%2CZAR%2CBRL%2CAED%2CPHP%2CDKK%2CPLN%2CTHB%2CIDR%2CHUF%2CSAR%2CMYR%2CCZK%2CCLP%2CILS%2CCOP%2CEGP%2CTWD%2CARS%2CVND%2CUAH%2CNGN%2CKES%2CPKR%2CIQD&base_currency=PLN"
+    );
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const { meta, data } = await response.json();
+
+    return {
+      success: true,
+      meta,
+      data,
+    };
+  } catch {
+    return {
+      success: false,
+    };
+  }
+};
